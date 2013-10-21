ced09bc4d4969f02153805d24ddd4538146425c1
Jason French <frenchja@gmail.com>
2013-10-21 17:10:24 -0500

    Added wiktionary template

---
+++ R/wikitionary.R
@@ -0,0 +1,31 @@
+# Wiktionary R API
+# Authors:  David M. Condon and Jason A. French <jason@sapa-project.org>
+
+wiki.login <- function(user,password){
+  require(RCurl)
+  https://en.wiktionary.org/w/api.php?action=login&lgname=user&lgpassword=password
+}
+
+wiki.logout <- function(){
+  https://en.wiktionary.org/w/api.php?action=logout
+}
+
+wiki.search <- function(x){
+  require(RCurl)
+  require(RJSONIO)
+  data <- getForm(
+    "https://en.wiktionary.org/w/api.php", 
+    action  = "opensearch", 
+    search  = x
+  )
+  return(fromJSON(data))
+}
+
+wiki.synonym <- function(x){
+  
+}
+
+wiki.antonym <- function(x){
+  
+}
+  
