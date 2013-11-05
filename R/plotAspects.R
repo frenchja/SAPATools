5a2a8385d17c14de1ae0e6e227dfc6389c7bba5e
Jason A. French <frenchja@u.northwestern.edu>
2013-11-05 12:02:24 -0600

    First working version.  Facet is broken though.

--- R/plotAspects.R
+++ R/plotAspects.R
@@ -6,28 +6,30 @@ plot.aspects <- function(aspect = 'Plasticity', sapa.data = IRTscores,
                          by = 'discipline', facet = FALSE){
   # This function plots SAPA aspects by discipline or major.
   # TODO:  
-  # 1. scale() mean.
+  # 1. Facet is broken..
   
+  by <<- by
   aspect <<- aspect
   sapa.data[[aspect]] <- scale(sapa.data[[aspect]])
+  
   # Check package
   if(!require(ggplot2)){
     stop('Install ggplot2 package.')
   }
   
-  # Facet majors by discipline for cleaner plot
-  if(isTRUE(facet) & by == 'major'){
-    facet <- 'discipline'
-  }
-  
   switch(by,
          'discipline'={
-           factor.labels <- c('Undecided','Arts','Business','Communications','Community and Social Services','Computer and Information Sciences',
+           discipline.labels <- c('Undecided','Arts','Business','Communications','Community and Social Services','Computer and Information Sciences',
                              'Cultural and Regional Studies','Education','Engineering and Technology','Language and Literature Studies',
                              'Mathematics','Medicine and Allied Health','Natural Sciences','Social Sciences')
+           discipline.levels <- c(0,1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2100,2200)
          },
          'major'={
-           factor.labels <- c('Art History','Art Theory and Practice','Arts Education ','Culinary Arts and Sciences',
+           discipline.labels <- c('Undecided','Arts','Business','Communications','Community and Social Services','Computer and Information Sciences',
+                                  'Cultural and Regional Studies','Education','Engineering and Technology','Language and Literature Studies',
+                                  'Mathematics','Medicine and Allied Health','Natural Sciences','Social Sciences')
+           discipline.levels <- c(0,1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2100,2200)
+           major.labels <- c('Undecided','Art History','Art Theory and Practice','Arts Education ','Culinary Arts and Sciences',
                               'Dance','Design and Applied Arts','Drama/Theater Arts','Fashion','Fine and Studio Arts',
                               'Graphic Arts','Interior Design','Landscape Design','Music','Music Education',
                               'Performance Studies','Other Performing or Visual Art Major','Accounting','Agricultural Businesses',
@@ -63,14 +65,28 @@ plot.aspects <- function(aspect = 'Plasticity', sapa.data = IRTscores,
                               'Oceanography','Physics','Other Natural Sciences Major','Anthropology','Criminology','Economics','Geography',
                               'Government','History','Law and Legal Studies','Philosophy','Political Science','Psychology','Religion',
                               'Sociology','Other Social Sciences Major')
+           major.levels <- c(0,1001,1002,1003,1004,1005,1006,1007,1008,1009,1010,1011,1012,1013,1014,1015,1016,1101,1102,1103,1104,1105,
+                              1106,1107,1108,1109,1110,1111,1112,1113,1114,1115,1201,1202,1203,1204,1205,1206,1207,1301,1302,1303,1304,1305,
+                              1306,1307,1308,1401,1402,1403,1404,1405,1406,1501,1502,1503,1504,1505,1506,1507,1508,1509,1510,1511,1512,1601,
+                              1602,1603,1604,1605,1606,1607,1608,1609,1610,1611,1612,1701,1702,1703,1704,1705,1706,1707,1708,1709,1710,1711,
+                              1712,1801,1802,1803,1804,1805,1806,1807,1808,1809,1810,1811,1812,1813,1814,1815,1816,1901,1902,1903,1904,1905,
+                              1906,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2101,2102,2103,2104,2105,2106,2107,2108,2109,2110,2111,
+                              2112,2113,2201,2202,2203,2204,2205,2206,2207,2208,2209,2210,2211,2212,2213)
          },
          'job'={
            factor.labels <- c()
          } )
   
   sapa.data[[by]] <- factor(as.factor(sapa.data[[by]]), 
-                                 levels=c(levels(as.factor(sapa.data[[by]]))),
-                                 labels=factor.labels) 
+                                 levels=get(paste(by,'.levels',sep='')),
+                                 labels=get(paste(by,'.labels',sep='')))
+  
+  if(isTRUE(facet)){
+    sapa.data[['discipline']] <- factor(as.factor(sapa.data[['discipline']]),
+                                        levels=discipline.levels,
+                                        labels=discipline.labels)
+  }
+  
   # Winston's Function
   summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
                         conf.interval=.95, .drop=TRUE) {
@@ -107,18 +123,37 @@ plot.aspects <- function(aspect = 'Plasticity', sapa.data = IRTscores,
     
     return(datac)
   }
+  if(isTRUE(facet)){
+    group.data <- summarySE(data = sapa.data, measurevar = aspect, 
+                            groupvars = c('discipline','major'), na.rm=TRUE)
+  }
+  else if(by == 'major'){
+    group.data <- summarySE(data = sapa.data, measurevar = aspect, 
+                            groupvars = 'major', na.rm=TRUE)
+  } else if(by == 'discipline') {
+    group.data <- summarySE(data = sapa.data, measurevar = aspect, 
+                            groupvars = 'discipline', na.rm=TRUE)
+  }
   
-  group.data <- summarySE(data = sapa.data, measurevar = aspect, 
-                          groupvars = 'discipline', na.rm=TRUE)
+  # Get rid of majors with NAs
+  group.data <- group.data[complete.cases(group.data),]
   
-  c <- ggplot(group.data, aes(x = reorder(discipline,get(aspect)), 
+  # Get rid of majors with < 20
+  group.data <- subset(x=group.data,subset=group.data$N > 20)
+  
+  c <- ggplot(group.data, aes(x = reorder(get(by),get(aspect)), 
                               y = get(aspect)))
   
   sapa.plot <- c + geom_pointrange(aes(ymin=get(aspect)-ci, 
                                        ymax=get(aspect)+ci),size=.8) + 
                 coord_flip() + 
-                xlab(label='Academic Discipline') + ylab(label=paste(aspect,'Mean')) + 
-                theme_bw() + 
+                xlab(label=paste('Academic',by)) + ylab(label=paste(aspect,'Mean')) + 
+                theme_bw(base_size=10) + 
                 geom_hline(aes(yintercept = 0),colour="#BB0000", linetype="dashed")
+  
+  if(isTRUE(facet)){
+  sapa.plot <- sapa.plot + facet_wrap(~ discipline)
+  }
+  
   print(sapa.plot)
 }
