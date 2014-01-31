4d8f900169418d54416202ff355eabb3442ec294
DavidCondon <david@sapa-project.org>
2014-01-31 14:40:46 -0600

    Update to PairwiseDescribe

--- R/PairwiseDescribe.R
+++ R/PairwiseDescribe.R
@@ -2,15 +2,8 @@ PairwiseDescribe <- function(x) {
 	# Author:  David M. Condon and Jason A. French
 	# Args:
 	#	  x:	SAPA data.frame
-	# Returns:  mean and sd of pairwise administrations
-	countXItems <- count.pairwise(x)
-	lowleft <- lower.tri(countXItems, diag = FALSE)
-	countXItems[!lowleft] <- NA
-	mean <- mean(countXItems, na.rm=TRUE)
-	median <- median(countXItems, na.rm=TRUE)
-	sd <- sd(countXItems, na.rm=TRUE)
-	min <- min(countXItems, na.rm=TRUE)
-	max <- max(countXItems, na.rm=TRUE)
-	results <- list(mean=mean, median=median, sd=sd, min=min, max=max)
-	return(results)
+	# Returns:  descriptives for pairwise administrations
+	cp <- count.pairwise(x,diagonal=FALSE)
+	cp <- as.vector(cp[lower.tri(cp)])
+	describe(cp,skew=FALSE)
 }
